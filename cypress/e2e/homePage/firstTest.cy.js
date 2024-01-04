76b61adb71585b0fb3859c9160740d09806597a8
Daquan McDaniel <developedbydmac@gmail.com>
2024-01-04 18:26:32 -0500

    update test and baseUrl

--- cypress/e2e/homePage/firstTest.cy.js
+++ cypress/e2e/homePage/firstTest.cy.js
@@ -1,7 +1,17 @@
 describe('Chick-fil-A Website Test', () => {
+    
+    
     it('Visits the Chick-fil-A Website', () => {
-        cy.visit('https://www.chick-fil-a.com/');
+        cy.visit('/');
         cy.title().should('include', 'Chick-fil-A');
         cy.get('#onetrust-accept-btn-handler').click();
+    });
+
+    it('Visits the Chick-fil-A Menu-Entrees', () => {
+        cy.visit('/#menu');
+        cy.get('#onetrust-accept-btn-handler').click();
+        cy.url().should('include','#menu');
+        cy.get('[href="/menu"] > :nth-child(1)').click();
+        cy.contains('Breakfast').click();
     });
 });
